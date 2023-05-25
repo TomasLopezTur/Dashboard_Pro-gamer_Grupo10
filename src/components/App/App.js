f9e9482db5615a9200b080d573d64e6d244c5e67
TomasLopezTur <tomas.lopez.tur@gmail.com>
2023-05-25 19:20:43 -0300

    Creando dashboard con react

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React DOC - inicio rapido!!</h1>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/App/App.test.js
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react'; /* , screen */
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  /* const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument(); */
+});
